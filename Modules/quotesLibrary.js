03ca70c0bd186849ecb2b36ad51898a5c8acb6f9
Joy <abundantjoy360@gmail.com>
2023-02-23 05:22:43 -0600

    "Added travel quotes to quotes library.js"

--- Modules/quotesLibrary.js
+++ Modules/quotesLibrary.js
@@ -1,105 +1,37 @@
-const quotes = {
-    travel:[
-            "",
-            "",
-            "",
-            "",
-            "",
-    ],
-
-    random:[
-
-    ],
-
-    intellectual:[
-
+const travelQuotes = [
+        "\"The world is a book and those who do not travel read only one page.\"" + " - \"Saint Augustine\"",
+        "\"Not all those who wander are lost.\"" + " - \"J.R.R. Tolkien\"",
+        "\"Travel is fatal to prejudice, bigotry, and narrow-mindedness.\"" + " - \"Mark Twain\"",
+        "\"The journey not the arrival matters.\"" + " - \"T.S. Eliot\"",
+        "\"We travel, some of us forever, to seek other states, other lives, other souls.\"" + " - \"Anaïs Nin\"",
+        "\"A journey of a thousand miles begins with a single step.\"" + " - \"Lao Tzu\"",
+        "\"Traveling – it leaves you speechless, then turns you into a storyteller.\"" + " - \"Ibn Battuta\"",
+        "\"Not until we are lost do we begin to understand ourselves.\"" + " - \"Henry David Thoreau\"",
+        "\"I haven't been everywhere, but it's on my list.\"" + " - \"Susan Sontag\"",
+        "\"Adventure is worthwhile.\"" + " - \"Aesop\"",
+        "\"Once a year, go someplace you've never been before.\"" + " - \"Dalai Lama\"",
+        "\"The gladdest moment in human life is a departure into unknown lands.\"" + " - \"Sir Richard Burton\"",
+        "\"To awaken alone in a strange town is one of the pleasantest sensations in the world.\"" + " - \"Freya Stark\"",
+        "\"I travel not to escape life, but for life not to escape me.\"" + " - \"Anonymous\"",
+        "\"To travel is to live.\"" + " - \"Hans Christian Andersen\"",
+        "\"I travel light. I think the most important thing is to be in a good mood and enjoy life, wherever you are.\"" + " - \"Diane von Fürstenberg\"",
+        "\"The world is too big to stay in one place and life is too short to do just one thing.\"" + " - \"Anonymous\"",
+        "\"Investment in travel is an investment in yourself.\"" + " - \"Matthew Karsten\"",
+        "\"I travel not to find myself but to rediscover who I've been all along.\"" + " - \"Anonymous\"",
+        "\"Travel makes one modest. You see what a tiny place you occupy in the world.\"" + " - \"Gustave Flaubert\"",
+        "\"Travel far enough, you meet yourself.\"" + " - \"David Mitchell\"",
+        "\"Travel is the only thing you can buy that makes you richer.\"" + " - \"Anonymous\"",
+        "\"The best education I have ever received was through travel.\"" + " - \"Lisa Ling\"",
+        "\"The world is a book, and those who do not travel read only a page.\"" + " - \"Attributed to Saint Augustine\"",
+        "\"Traveling is like flirting with life. It's like saying, 'I would stay and love you, but I have to go; this is my station.'\"" + " - \"Lisa St. Aubin de Terán\"",
+        "\"One's destination is never a place, but a new way of seeing things.\"" + " - \"Henry Miller\"",
+        "\"Jet lag is for amateurs.\"" + " - \"Dick Clark\"",
+        "\"Life is short and the world is wide.\"" + " - \"Simon Raven\"",
+        "\"The impulse to travel is one of the hopeful symptoms of life.\"" + " - \"Agnes Repplier\"",
+        "\"Travel far enough, you meet yourself.\""+ " - \"Joseph Campbell\"",
+        "\"We travel for romance, we travel for architecture, and we travel to be lost.\"" + " - \"Ray Bradbury\"",
+        "\"The world is full of wonderful things you haven't seen yet. Don't ever give up on the chance of seeing them.\"" + " - \"J.K. Rowling\"",
+        "\"Do not follow where the path may lead. Go instead where there is no path and leave a trail.\"" + " - \"Ralph Waldo Emerson\"",
+        "\"Wherever you go becomes a part of you somehow.\"" + " - \"Anita Desai\"",
+        "\"A good traveler has no fixed plans and is not intent on arriving.\"" + " - \"Lao Tzu\""
     ]
-
-}
-
-
-// “The world is a book and those who do not travel read only one page.” – Saint Augustine
-// “Travel is fatal to prejudice, bigotry, and narrow-mindedness.” – Mark Twain
-// “Not all those who wander are lost.” – J.R.R. Tolkien
-// “Travel far enough, you meet yourself.” – David Mitchell
-// “I haven’t been everywhere, but it’s on my list.” – Susan Sontag
-// “A journey is best measured in friends, rather than miles.” – Tim Cahill
-// “To travel is to live.” – Hans Christian Andersen
-// “Adventure is worthwhile.” – Aesop
-// “I travel not to escape life, but for life not to escape me.” – Anonymous
-// “Life is short and the world is wide.” – Simon Raven
-// “Travel makes one modest. You see what a tiny place you occupy in the world.” – Gustave Flaubert
-// “We travel, some of us forever, to seek other states, other lives, other souls.” – Anaïs Nin
-// “Traveling – it leaves you speechless, then turns you into a storyteller.” – Ibn Battuta
-// “The journey not the arrival matters.” – T.S. Eliot
-// “We travel for romance, we travel for architecture, and we travel to be lost.” – Ray Bradbury
-// “Once a year, go someplace you’ve never been before.” – Dalai Lama
-// “The world is big and I want to have a good look at it before it gets dark.” – John Muir
-// “Travel is the only thing you can buy that makes you richer.” – Anonymous
-// “If you think adventure is dangerous, try routine; it’s lethal.” – Paulo Coelho
-// “Jet lag is for amateurs.” – Dick Clark
-// “Travel brings power and love back into your life.” – Rumi
-// “I love the feeling of being anonymous in a city I’ve never been before.” – Anonymous
-// “Traveling is a brutality. It forces you to trust strangers and to lose sight of all that familiar comfort of home and friends. You are constantly off balance.” – Cesare Pavese
-// “I never travel without my diary. One should always have something sensational to read in the train.” – Oscar Wilde
-// “Wherever you go becomes a part of you somehow.” – Anita Desai
-// “Travel and change of place impart new vigor to the mind.” – Seneca
-// “Wanderlust: n. a strong desire for or impulse to wander or travel and explore the world.” – Anonymous
-// “A good traveler has no fixed plans and is not intent on arriving.” – Lao Tzu
-// “Two roads diverged in a wood and I – I took the one less traveled by.” – Robert Frost
-// “Travel is glamorous only in retrospect.” – Paul Theroux
-
-
-// "Shoot for the moon. Even if you miss, you'll land among the stars." - Les Brown
-
-// "The moon is a friend for the lonesome to talk to." - Carl Sandburg
-
-// "The moon is a loyal companion. It never leaves. It’s always there, watching, steadfast, knowing us in our light and dark moments, changing forever just as we do." - Tahereh Mafi
-
-// "The moon is a silver pin-head vast, That holds the heaven's tent-hangings fast." - William R. Alger
-
-// "The moon is a white strange world, great, white, soft-seeming globe in the night sky, and what she actually communicates to me across space I shall never fully know." - D.H. Lawrence
-
-// "The moon was a ghostly galleon tossed upon cloudy seas." - Alfred Noyes
-
-// "The moon is the reflection of your heart and moonlight is the twinkle of your love." - Debasish Mridha
-
-// "Everyone is a moon, and has a dark side which he never shows to anybody." - Mark Twain
-
-// "The moon lives in the lining of your skin." - Pablo Neruda
-
-// "I'd like to be a queen of people's hearts, in people's hearts, but I don't see myself being Queen of this country. I don't think many people would want me to be Queen." - Princess Diana
-
-// Sun Quotes:
-
-// "Keep your face to the sunshine and you cannot see a shadow." - Helen Keller
-
-// "A flower cannot blossom without sunshine, and man cannot live without love." - Max Muller
-
-// "The sun is a daily reminder that we too can rise again from the darkness, that we too can shine our own light." - S. Ajna
-
-// "The sun will rise and set regardless. What we choose to do with the light while it's here is up to us." - Alexandra Elle
-
-// "The sun does not shine for a few trees and flowers, but for the wide world's joy." - Henry Ward Beecher
-
-// "We are all like the bright moon, we still have our darker side." - Khalil Gibran
-
-// "Keep your face always toward the sunshine - and shadows will fall behind you." - Walt Whitman
-
-// "The sun is a daily reminder that we too can rise again from the darkness, that we too can shine our own light." - S. Ajna
-
-// "The sun, with all those planets revolving around it and dependent on it, can still ripen a bunch of grapes as if it had nothing else in the universe to do." - Galileo Galilei
-
-// "The sun is a daily reminder that we too can rise again from the darkness, that we too can shine our own light." - S. Ajna
-
-// Star Quotes:
-
-// "We are all in the gutter, but some of us are looking at the stars." - Oscar Wilde
-
-// "The nitrogen in our DNA, the calcium in our teeth, the iron in our blood, the carbon in our apple pies were made in the interiors of collapsing stars. We are made of starstuff." - Carl Sagan
-
-// "I will love the light for it shows me the way, yet I will endure the darkness because it shows me the stars." - Og Mandino
-
-// "We are all in the gutter, but some of us are looking at the stars." - Oscar Wilde
-
-// "At night, I
